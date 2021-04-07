5154d3127ceee1347273c3c423977f32217ca5b3
SerJLemast <maksiomrpp@gmail.com>
2021-04-07 11:25:00 +0300

    create workspace

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<my-component />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import MyComponent from './components/MyComponent'
+import MyComponent from './components/MyComponent.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyComponent
+    
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
